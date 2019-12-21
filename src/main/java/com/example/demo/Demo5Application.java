e22e3bc32e6552faa9a67e5b598e0f5618324002
nnp-web <nnp508615@126.com>
2019-12-21 12:10:09 +0800

    注入不到security的默认处理”

--- src/main/java/com/example/demo/Demo5Application.java
+++ src/main/java/com/example/demo/Demo5Application.java
@@ -2,10 +2,21 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
 
-@SpringBootApplication
+@SpringBootApplication(exclude=DataSourceAutoConfiguration.class)
+@EnableWebSecurity
 public class Demo5Application {
 
+	@Bean
+    public RedirectStrategy redirectStrategy() {
+    	return new DefaultRedirectStrategy();
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Demo5Application.class, args);
 	}
